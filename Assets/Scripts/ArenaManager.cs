982199316264d3c85fa5cab08c386a1fef6684ea
scr3amind <screamindarko@gmail.com>
2021-03-12 01:04:14 -0600

    Agregando generador de posiciones

--- Assets/Scripts/ArenaManager.cs
+++ Assets/Scripts/ArenaManager.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class ArenaManager : MonoBehaviour
 {
+    [SerializeField] List<Transform> spawnPoints = new List<Transform>();
     private void Awake() 
     {
         GameManager.instance.initializePlayers();
+        randomizePlayerPositions();
+    }
+
+    private void randomizePlayerPositions()
+    {
+        if(spawnPoints.Count < GameManager.instance.numOfActivePlayers)
+        {
+            Debug.LogWarning("Insuficient SpawnPoints");
+            return;
+        }
+        else
+        {
+            List<Vector3> randomPositions = new List<Vector3>();
+            for (int i = 0; i < GameManager.instance.numOfActivePlayers; i++)
+            {
+                Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
+                randomPositions.Add(randomSpawnPoint.position);
+                spawnPoints.Remove(randomSpawnPoint);
+            }
+            GameManager.instance.setPlayersPositions(randomPositions);
+        }
+        
     }
 }
